56fbf9b54de20ae1517416a2fffe90602bf1dc35
Taylor <taylordavids997@gmail.com>
2023-11-19 11:56:57 +0200

    add function to components

---
+++ TAYABRA277_FTOW2306_GroupA_TaylorAbrahams_FinalCapstoneProject/src/Filter.jsx
@@ -0,0 +1,9 @@
+export default function Filter(){
+    return (
+        <>
+            <section className="hero--banner">
+                <h1>Test</h1>
+            </section>
+        </>
+    )
+}
---
+++ TAYABRA277_FTOW2306_GroupA_TaylorAbrahams_FinalCapstoneProject/src/Navbar.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Navbar() {
+    return(
+        <>
+            <nav className="navbar--section">
+                <h2>Welcome Back !!!</h2>
+                <ul className="navbar--container">
+                    <li className="navbar--listitems">Aboutme</li>
+                    <li className="navbar--listitems">Music</li>
+                    <li className="navbar--listitems">Price</li>
+                </ul>
+            </nav>
+        </>
+    )
+}
