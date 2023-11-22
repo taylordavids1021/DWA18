67a930ea97b3b56228e6859f3f3e1fa6c79a523c
Taylor <taylordavids997@gmail.com>
2023-11-22 12:13:28 +0200

    add icon images

---
+++ TAYABRA277_FTOW2306_GroupA_TaylorAbrahams_FinalCapstoneProject/src/SocialMediaLinks.jsx
@@ -0,0 +1,12 @@
+export default function SocialMediaLinks() {
+     
+    return (
+        <>
+            <div className="socialMediaLinks--Icons">
+                <img src="./images/facebook.png" className="icon--image"/>
+                <img src="./images/tik-tok.png" className="icon--image"/>
+                <img src="./images/instagram.png" className="icon--image"/>
+            </div>
+        </>
+    )
+}
