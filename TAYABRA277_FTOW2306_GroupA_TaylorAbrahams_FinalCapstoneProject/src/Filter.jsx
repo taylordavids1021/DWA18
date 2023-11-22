1b245f503cbe50c6af55f40c6fe9a10031aee76d
Taylor <taylordavids997@gmail.com>
2023-11-22 12:12:40 +0200

    add filter form

--- TAYABRA277_FTOW2306_GroupA_TaylorAbrahams_FinalCapstoneProject/src/Filter.jsx
+++ TAYABRA277_FTOW2306_GroupA_TaylorAbrahams_FinalCapstoneProject/src/Filter.jsx
@@ -2,7 +2,21 @@ export default function Filter(){
     return (
         <>
             <section className="hero--banner">
-                <h1>Test</h1>
+                <h1 className="filter--title">Podcast</h1>
+                <div className="search--pannel--section">
+                    <input 
+                        type="text"
+                        placeholder="Filter"
+                        className="form--input"
+                    />
+                    <button 
+                        className="form--button"
+                        // onClick={getMemeImage}
+                    >
+                        Search🖼
+                    </button>
+                </div>
+                <div></div>
             </section>
         </>
     )
