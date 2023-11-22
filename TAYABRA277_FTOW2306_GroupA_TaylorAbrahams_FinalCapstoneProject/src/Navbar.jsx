482468379cab814e9f0316ac1bd46be547a3ce91
Taylor <taylordavids997@gmail.com>
2023-11-22 12:13:18 +0200

    edited navbar list items

--- TAYABRA277_FTOW2306_GroupA_TaylorAbrahams_FinalCapstoneProject/src/Navbar.jsx
+++ TAYABRA277_FTOW2306_GroupA_TaylorAbrahams_FinalCapstoneProject/src/Navbar.jsx
@@ -4,9 +4,9 @@ export default function Navbar() {
     return(
         <>
             <nav className="navbar--section">
-                <h2>Welcome Back !!!</h2>
+                <h2 className="navbar--title">Welcome Back !!!</h2>
                 <ul className="navbar--container">
-                    <li className="navbar--listitems">Aboutme</li>
+                    <li className="navbar--listitems">About</li>
                     <li className="navbar--listitems">Music</li>
                     <li className="navbar--listitems">Price</li>
                 </ul>
