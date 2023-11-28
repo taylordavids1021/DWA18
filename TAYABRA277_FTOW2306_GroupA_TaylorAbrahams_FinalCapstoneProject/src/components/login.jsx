76dbf1e2f7b0119430349d822e82aa83b29d5b27
Taylor <taylordavids997@gmail.com>
2023-11-28 15:12:08 +0200

    create login function

--- TAYABRA277_FTOW2306_GroupA_TaylorAbrahams_FinalCapstoneProject/src/components/login.jsx
+++ TAYABRA277_FTOW2306_GroupA_TaylorAbrahams_FinalCapstoneProject/src/components/login.jsx
@@ -1,14 +1,11 @@
 import { supabase } from './SupabaseClient';
-/**a login page is a fundamental component of online security and user management. 
- * It helps protect sensitive information, provides a personalized user experience, 
- * and ensures that users are granted appropriate access to resources within a system. 
- * */
+
 // Define the Login component
 const Login = () => {
   // Function to handle login using GitHub
   const handleLogin = async () => {
     try {
-      // this is where I attempted to sign in using Supabase authentication with GitHub provider
+      // this is where i attempted to sign in using Supabase authentication with GitHub provider
       await supabase.auth.signIn({
         provider: 'github',
       });
