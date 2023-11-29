52612c0b6b620450e95b53b83be15988d046902f
Taylor <taylordavids997@gmail.com>
2023-11-30 00:10:25 +0200

    lmplement history for last time and date seen etc function/template

---
+++ myPodcast/src/components/History.jsx
@@ -0,0 +1,114 @@
+import { useState, useEffect } from 'react';
+
+const History = () => {
+  /**
+   * State to store listening history and the last listened episode
+   * 
+   * This code essentially sets up a React state variable 
+   * (listeningHistory) with an initial value based on the data stored in localStorage
+   */
+  const [listeningHistory, setListeningHistory] = useState(
+    JSON.parse(localStorage.getItem('listeningHistory')) || []
+  );
+  const [lastListened, setLastListened] = useState(
+    JSON.parse(localStorage.getItem('lastListened')) || {}
+  );
+
+  /**
+   * Effect to update 'listeningHistory' in local storage when it changes
+   * 
+   * This setup ensures that the local storage is updated whenever 
+   * listeningHistory changes, helping to keep the local storage in sync with the component's state.
+   */
+  useEffect(() => {
+    localStorage.setItem('listeningHistory', JSON.stringify(listeningHistory));
+  }, [listeningHistory]);
+
+  /**
+   * Effect to update 'lastListened' in local storage and add to history
+   * 
+   * useEffect hook in a React component, and its purpose is to update the local storage whenever 
+   * the lastListened variable changes. This is similar to the previous example you provided for 
+   * updating the listeningHistory in local storage.
+   */
+  useEffect(() => {
+    localStorage.setItem('lastListened', JSON.stringify(lastListened));
+
+ /** 
+  * Create a timer to add to history after 10 minutes of no activity
+  * 
+  * This callback function checks if certain conditions are met before updating the listening history.
+  * 
+  * If the conditions are met, it uses the setListeningHistory function to update the listening history state. 
+  * It creates a new array with the previous history (prevHistory) and adds a new object representing the 
+  * latest listened item.
+  * 
+  * The new object includes the show, episode, progress, and a timestamp representing the current date and 
+  * time in ISO format
+ */
+ const timer = setTimeout(() => {
+  if (lastListened.show && lastListened.episode && lastListened.progress) {
+    setListeningHistory((prevHistory) => [
+      ...prevHistory,
+      {
+        show: lastListened.show,
+        episode: lastListened.episode,
+        progress: lastListened.progress,
+        timestamp: new Date().toISOString(),
+      },
+    ]);
+  }
+}, 10 * 60 * 1000);
+/**
+ * The return statement indicates that the function inside it will be used as the cleanup function.
+ * The arrow function () => clearTimeout(timer) is the cleanup function itself.
+ * clearTimeout(timer) is called to clear the timeout identified by the timer variable
+ * 
+ * Clean up the timer if component unmounts or 'lastListened' changes
+ */
+return () => clearTimeout(timer);
+/**
+ * This structure ensures that the timer is cleared if the 
+ * component is unmounted or if lastListened changes before the timeout expires, 
+ * preventing potential issues related to state updates on an unmounted component
+ */
+}, [lastListened]);
+
+  /** Function to reset listening history and last listened episode 
+  */
+  const handleResetProgress = () => {
+    setListeningHistory([]);
+    setLastListened({});
+  };
+
+  /**
+   *  Render the History component
+  */
+  return (
+    <div className="history-container">
+      <h1>Listening History</h1>
+      {listeningHistory.length > 0 ? (
+        <ul>
+          {listeningHistory.map((episode, index) => (
+            <li key={index}>
+              <p>Show: {episode.show}</p>
+              <p>Episode: {episode.episode}</p>
+              <p>Progress: {episode.progress} seconds</p>
+              <p>Timestamp: {episode.timestamp}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No listening history found.</p>
+      )}
+      <button onClick={handleResetProgress}>Reset Listening Progress</button>
+    </div>
+  );
+};
+
+export default History;
+/**
+ * Displays a user's listening history.
+ * Lists the podcasts and episodes they've listened to, along with progress and timestamps.
+ * Users can reset their listening history.
+ */
