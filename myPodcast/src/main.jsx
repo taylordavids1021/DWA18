cd6b41aed37e09cc1dd81bb231040b75dcccc4cd
Taylor <taylordavids997@gmail.com>
2023-12-01 02:25:01 +0200

    attempted react router

--- myPodcast/src/main.jsx
+++ myPodcast/src/main.jsx
@@ -1,3 +1,16 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import App from './App.jsx';
+// import './index.css'
+
+// ReactDOM.render(
+//   <Router>
+//     <App />
+//   </Router>,
+//   document.getElementById('root')
+// );
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
