b674d4d8374c9e854dd2db156c5b01f6f13c1f37
Taylor <taylordavids997@gmail.com>
2023-12-01 14:09:21 +0200

    attepmted react router to link pages

--- myPodcast/src/main.jsx
+++ myPodcast/src/main.jsx
@@ -1,16 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { BrowserRouter as Router } from 'react-router-dom';
-// import App from './App.jsx';
-// import './index.css'
-
-// ReactDOM.render(
-//   <Router>
-//     <App />
-//   </Router>,
-//   document.getElementById('root')
-// );
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -21,3 +8,27 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import React, { createRoot } from 'react';
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import App from './App.jsx';
+// import './index.css'
+
+/**
+ * React 18 introduces concurrent rendering improvements and 
+ * a new root API called createRoot to enable these features. 
+ * The deprecation warning you're seeing suggests that ReactDOM.render will be 
+ * replaced by createRoot in React 18.
+ */
+
+// const root = createRoot(<Router />, <App />, document.getElementById('root'));
+// root.render(<App />);
+
+// ReactDOM.render(
+//   <Router>
+//     <App />
+//   </Router>,
+//   document.getElementById('root')
+// );
